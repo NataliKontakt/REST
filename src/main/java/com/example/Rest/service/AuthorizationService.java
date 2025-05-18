eac974d889a636f0b35ff7ebbed28cbad381a32b
natali-kontakt <natalikontaktgood@gmail.com>
2025-05-18 20:34:00 +0500

    add docker

--- src/main/java/com/example/Rest/service/AuthorizationService.java
+++ src/main/java/com/example/Rest/service/AuthorizationService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class AuthorizationService {
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public AuthorizationService(UserRepository userRepository) {
         this.userRepository = userRepository;
